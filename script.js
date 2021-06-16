7c341b67fd168994710dc3e05557dfadb5250861
Karen Marting <karenannemarting@yahoo.com>
2021-06-15 19:18:13 -0700

    first timeslot saved

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 
-const date = new Date();
-const day = moment().format('dddd MMMM Do YYYY h:mm:ss a');
-document.getElementById("currentDay").innerHTML = day;
+    const date = new Date();
+    const day = moment().format('dddd MMMM Do YYYY h:mm:ss a');
+    document.getElementById("currentDay").innerHTML = day;
 
-
-function saveTasks(){
-const textArea = document.querySelector(".textarea");
-const btn = document.querySelector(".saveBtn");
-let task = localStorage.getItem("task");
-let hourlyTask = JSON.parse(task) || [];
-
-if(task){
-    hourlyTask = JSON.parse(task);
-    } else {
-    hourlyTask = [];
-    }
-
-for (let i = 0; i < hourlyTask.length; i++){
-    textArea.textContent = hourlyTask[i]
-    }
+    const textArea = document.querySelector("textarea");
+    const btn = document.querySelector(".saveBtn");
 
 btn.addEventListener("click", function(event){
     event.preventDefault()
-    const currentValue = textArea.value
+
+    const currentValue = hour8.value
     hourlyTask.push(currentValue)
-    localStorage.setItem("task".JSON.stringify(hourlyTask))
+    localStorage.setItem("hour8",JSON.stringify(hourlyTask))
     window.location.reload()
-    });
+});
+    
+    const timeSlots = ["hour8", "hour9", "hour10", "hour11", "hour12","hour13", "hour14", "hour15", "hour16", "hour17"] 
+    
+    let task = localStorage.getItem("hour8");
+    let hourlyTask = JSON.parse(task) || [];
+
+function loadSavedTasks(){
+    for (let i = 0; i < hourlyTask.length; i++){
+        textArea.textContent = hourlyTask[i]
+        if(task){
+    hourlyTask = JSON.parse(task);
+        } else {
+            hourlyTask = [];
+        }
+    }
 }
-saveTasks();
+loadSavedTasks();
 
 
 
